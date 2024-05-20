832d5a5f034938aab523868a69d98c02ce8a5d4e
Sumit Mahi <128840594+mahisumit@users.noreply.github.com>
2024-05-20 12:05:33 +0200

    Equacions.java

---
+++ practica_REFAC/src/Equacions.java
@@ -0,0 +1,21 @@
+public class Equacions {
+    public static boolean maxim(int a, int b) {
+        return a > b;
+    }
+
+    public static void calcEquacioSegongrau(double a, double b, double c) {
+        double D = b * b - 4 * a * c;
+        if (D > 0) {
+            double x1, x2;
+            x1 = (-b - Math.sqrt(D)) / (2 * a);
+            x2 = (-b + Math.sqrt(D)) / (2 * a);
+            System.out.println("x1 = " + x1 + ", x2 = " + x2);
+        } else if (D == 0) {
+            double x;
+            x = -b / (2 * a);
+            System.out.println("x = " + x);
+        } else {
+            System.out.println("L'equació no té arrels reals");
+        }
+    }
+}
